53465c4fd9d7e615487d2211c1262af77ed15ece
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2022-11-21 15:05:55 -0300

    adicionando contexto do banco de dados

--- quizAPI/quizAPI/Program.cs
+++ quizAPI/quizAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using QuizAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<QuizDbContext>(options => 
+    options.UseMySql(builder.Configuration.GetConnectionString("DevConnection"),
+    ServerVersion.Parse("8.0.30")
+));
 
 var app = builder.Build();
 
