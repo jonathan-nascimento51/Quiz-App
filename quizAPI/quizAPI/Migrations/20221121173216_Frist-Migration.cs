785a7faff8f14831a505e70e7e882a9541ec09d8
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2022-11-21 15:14:11 -0300

    pacotes nuGets instalados

---
+++ quizAPI/quizAPI/Migrations/20221121173216_Frist-Migration.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace quizAPI.Migrations
+{
+    public partial class FristMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Participants",
+                columns: table => new
+                {
+                    ParticipantId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Score = table.Column<int>(type: "int", nullable: false),
+                    TimeTaken = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participants", x => x.ParticipantId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    QnId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    QnInWords = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Option1 = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Option2 = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Option3 = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Option4 = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Answer = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.QnId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Participants");
+
+            migrationBuilder.DropTable(
+                name: "Questions");
+        }
+    }
+}
